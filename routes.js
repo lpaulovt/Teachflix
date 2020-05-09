4107b1741f0e1ccdbaeff1ef9ec71f34cf6a7f4f
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-08 22:02:55 -0300

    chore: validating data

--- routes.js
+++ routes.js
@@ -18,6 +18,13 @@ routes.get('/teachers/create', function(req, res){
 })
 
 routes.post('/teachers', function(req, res){
+    const keys = Object.keys(req.body)
+    
+    for(key of keys){
+        if(req.body[key] ==""){
+            return res.send("Preencha todos os campos!")
+        }
+    }
     return res.send(req.body)
 })
 module.exports = routes
